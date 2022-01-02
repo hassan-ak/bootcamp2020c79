55e49aa03c0c39628dae62c262f348d29e2d6181
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-03 00:36:36 +0500

    lambda handler

---
+++ step39_aurora-serverless-data-api/aurora-serverless-data-api/lambda/index.ts
@@ -0,0 +1,69 @@
+import { env } from 'process';
+import * as AWS from 'aws-sdk';
+var rdsdataservice = new AWS.RDSDataService();
+
+interface QueryParams {
+  resourceArn: string;
+  secretArn: string;
+  database: string;
+  sql: string;
+}
+
+export async function handler(event: any, context: any) {
+  try {
+    // Defining parameters for rdsdataservice
+    const dbcarn = env.CLUSTER_ARN || '';
+    const dbsarn = env.SECRET_ARN || '';
+    var params: QueryParams = {
+      resourceArn: dbcarn,
+      secretArn: dbsarn,
+      database: 'mydb',
+      sql: '',
+    };
+    params['sql'] =
+      'CREATE TABLE IF NOT EXISTS records (recordid INT PRIMARY KEY, title VARCHAR(255) NOT NULL, release_date DATE);';
+    const data = await rdsdataservice.executeStatement(params).promise();
+    var body = {
+      records: data,
+    };
+
+    // //     second query
+    //     var params: QueryParams = {
+    //       resourceArn: dbcarn,
+    //       secretArn: dbsarn,
+    //       database: "new",
+    //       sql:
+    //         "INSERT INTO records(recordid,title,release_date) VALUES(001,'Liberian Girl','2012-05-03');",
+    //     };
+
+    //     const data2 = await rdsdataservice.executeStatement(params).promise();
+    //        var body = {
+    //       records: data2,
+    //     };
+
+    // //     third query
+    //     var params: QueryParams = {
+    //       resourceArn: dbcarn,
+    //       secretArn: dbsarn,
+    //       database: "new",
+    //       sql: "select * from records;",
+    //     };
+    //     const data3 = await rdsdataservice.executeStatement(params).promise();
+    //     var body = {
+    //       records: data3,
+    //     };
+
+    return {
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify(body),
+    };
+  } catch (error) {
+    console.log(error, 'error');
+    return {
+      statusCode: 400,
+      headers: {},
+      body: `Error creating table`,
+    };
+  }
+}
